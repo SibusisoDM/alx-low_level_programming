23eb2b3169e6ab5ee128fbff031fb4b1d0f2d74f
SibusisoDM <sdmangwane@gmail.com>
2022-09-12 13:17:29 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,7 +18,7 @@ for (ten = (hundred + 1); ten <= '9'; ten++) /*tens=100s+1*/
 for (one = (ten + 1); one <= '9'; one++) /*ones*/
 {
 putchar(hundred);
-putchar(ten0;
+putchar(ten);
 putchar(one);
 if (hundred != '7' || ten != '8' || one != '9')
 {
