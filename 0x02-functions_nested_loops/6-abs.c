bd35861565091c62b807ac5e0218e3340748d4f3
SibusisoDM <sdmangwane@gmail.com>
2022-09-14 03:57:30 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute function of the number
+ * @n: number to be evaluated
+ * Return: absolute value of the number
+ */
+
+int _abs(int n)
+{
+if (n < 0)
+return (-n);
+else
+return (n);
+}
