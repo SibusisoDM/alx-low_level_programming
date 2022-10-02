e8b8ca86c92ab0cc936446055fbb96c064594fa7
SibusisoDM <sdmangwane@gmail.com>
2022-10-02 07:53:53 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,13 +7,13 @@
  * Return: always 0
  */
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
 int i, j, sum;
 sum = 0;
-for (i = 1; i < argc ; i++)
+for (i = 1; i < argc; i++)
 {
-for (j = 0; argv[i][j] != '\0' ; j++)
+for (j = 0; argv[i][j] != '\0'; j++)
 {
 if (argv[i][j] < 47 || argv[i][j] > 57)
 {
