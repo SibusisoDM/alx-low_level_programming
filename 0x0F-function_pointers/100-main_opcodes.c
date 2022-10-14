6fbd3cd4ea13c828d13078e4098b7adb01571e22
SibusisoDM <sdmangwane@gmail.com>
2022-10-14 09:24:16 -0700

    done

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -6,26 +6,30 @@
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	short bytes, x;
-
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
-	bytes = atoi(argv[1]);
-	if (bytes < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-	printf("%02x", *((unsigned char *) (main)));
-	for (x = 1; x < bytes; ++x)
-		printf(" %02", *((unsigned char *) (main + x)));
-	putchar('\n');
-	exit(EXIT_SUCCESS);
+int bytes, i;
+char *arr;
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+bytes = atoi(argv[1]);
+if (bytes < 0)
+{
+printf("Error\n");
+exit(2);
+}
+arr = (char *)main;
+for (i = 0; i < bytes; i++)
+{
+if (i == bytes - 1)
+{
+printf("%02hhx ", arr[i]);
+}
+return (0);
+}
 }
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -24,10 +24,9 @@ i = 0;
 
 while (ops[i].op)
 {
-	if (strcmp(ops[i].op, s) == 0)
-		return (ops[i].f);
-	i++;
+if (strcmp(ops[i].op, s) == 0)
+return (ops[i].f);
+i++;
 }
-
 return (NULL);
 }
